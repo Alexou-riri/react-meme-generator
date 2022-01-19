0646d871ad4b79dca47d4df2008fdb807c82f688
Alex <alexandra.rieux@gmx.com>
2022-01-19 17:26:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+           Meme generator
+        </h1>
+        <h2> Have fun ! </h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
